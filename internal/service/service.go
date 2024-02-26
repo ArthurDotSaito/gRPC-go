5d00e8e01948576be1efe6647ba3c2c41391301e
Arthur Saito <arthur_saito1998@hotmail.com>
2024-02-26 18:26:21 -0300

    [feat] working with streams

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 
 	"github.com/ArthurDotSaito/gRPC-go/internal/database"
 	"github.com/ArthurDotSaito/gRPC-go/internal/pb"
@@ -50,4 +51,41 @@ func (c *CategoryService) ListCategories(ctx context.Context, in *pb.Empty) (*pb
 	}
 
 	return &pb.CategoryListResponse{Categories: categoryResponses}, nil
+}
+
+func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetRequest) (*pb.Category, error) {
+	category, err := c.CategoryDB.FindByCourseID(in.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Category{
+		Id: category.ID,
+		Name: category.Name,
+		Description: category.Description,
+	}, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error{
+	categories := &pb.CategoryListResponse{}
+
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(categories);
+		}
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id: categoryResult.ID,
+			Name: categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+	}
 }
